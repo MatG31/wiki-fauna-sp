052d8956d54c4cfb87aa377969b6fdcbd6e44525
Matheus <114451641+MatG31@users.noreply.github.com>
2024-09-08 15:55:09 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+function pesquisar() {
+
+    // Seleciona a seção HTML onde os resultados da pesquisa serão exibidos.
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    console.log(campoPesquisa);
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    if (campoPesquisa == ""){
+        section.innerHTML = `<p> Preencha o campo antes de pesquisar. </p>`
+        return;       
+    }
+
+    // Inicializa uma string vazia para armazenar o HTML dos resultados.
+    let resultados = "";
+    let nomeMin = "";
+    let dietaMin = "";
+    let habitatMin = "";
+    let curiosidadeMin = "";
+    let marcadoresMin = "";
+
+    // Itera sobre cada animal no array 'infoFauna'.
+    for (let blocoInf of infoFauna) {
+        nomeMin = blocoInf.nome.toLowerCase();
+        dietaMin = blocoInf.dieta.toLowerCase();
+        habitatMin = blocoInf.habitat.toLowerCase();
+        curiosidadeMin = blocoInf.curiosidade.toLowerCase();
+        marcadoresMin = blocoInf.marcadores.toLowerCase();
+        if (nomeMin.includes(campoPesquisa) || dietaMin.includes(campoPesquisa) || habitatMin.includes(campoPesquisa) || curiosidadeMin.includes(campoPesquisa) || marcadoresMin.includes(campoPesquisa)) {
+        // Concatena a string 'resultados' com uma nova div que representa um resultado da pesquisa.
+        // Utiliza template literals (``) para facilitar a construção da string e inserir as propriedades do objeto.
+            resultados += `<div class="item-resultado"> <h2> ${blocoInf.nome} </h2> <p class="descricao-meta"> ${blocoInf.habitat} <br> ${blocoInf.dieta} <br> ${blocoInf.curiosidade} </p> <a href="${blocoInf.link}" target="_blank"> Para saber mais clique aqui </a> </div>`;
+        } 
+
+        if (!resultados) {
+            resultados = `<p> O animal pesquisado não foi encontrado. </p>`
+        }
+    }
+
+    // Substitui o conteúdo HTML da seção "resultados-pesquisa" pelos resultados gerados.
+    section.innerHTML = resultados;
+}
+
+
---
+++ dados.js
@@ -0,0 +1,39 @@
+let infoFauna = [
+    // Cria um array (lista) chamado 'infoFauna' para armazenar informações sobre diferentes animais.
+    // Cada elemento do array é um objeto que representa um animal.
+
+    {
+        // Primeiro objeto do array, representando o Peixe-Opah.
+        nome: "Peixe-Opah", // Nome do animal
+        habitat: "Vive em: águas profundas", // Habitat do animal
+        dieta: "Alimenta-se de: lulas e outros cefalópodes", // Dieta do animal
+        curiosidade: "Pode manter seu corpo mais quente que a água circundante", // Curiosidade sobre o animal
+        link: "https://g1.globo.com/natureza/noticia/2015/05/cientistas-identificam-primeiro-peixe-de-sangue-quente.html", // Link para mais informações
+        marcadores: "aquático marinho raro"
+    },
+    // Os demais objetos seguem a mesma estrutura, representando outros animais.
+    {
+        nome: "Capivara",
+        habitat: "Vive em: margens de corpos d'água",
+        dieta: "Alimenta-se de: gramíneas, plantas aquáticas e frutas",
+        curiosidade: "É o maior roedor do mundo",
+        link: "https://capital.sp.gov.br/web/meio_ambiente/w/servicos/fauna/especies_da_cidade/8907",
+        marcadores: "fofo mamífero cidade"
+    },
+    {
+        nome: "Jararaca",
+        habitat: "Vive em: florestas tropicais e mata atlântica",
+        dieta: "Alimenta-se de: roedores, anfíbios, aves e répteis",
+        curiosidade: "A ponta de sua cauda quando filhote é geralmente de cor mais clara e ela a utiliza como um isca",
+        link: "https://capital.sp.gov.br/web/meio_ambiente/w/servicos/fauna/especies_da_cidade/13426",
+        marcadores: "cobra venenosa perigoso réptil"
+    },
+    {
+        nome: "Sabiá-laranjeira",
+        habitat: "Vive em: bordas de matas",
+        dieta: "Alimenta-se de: frutas, insetos e outros invertebrados",
+        curiosidade: "Assim como os humanos, os sabiás-laranjeiras aprendem e imitam os cantos de outros pássaros, desenvolvendo um repertório pessoal ao longo de suas vidas",
+        link: "https://capital.sp.gov.br/web/meio_ambiente/w/servicos/fauna/especies_da_cidade/13230",
+        marcadores: "pássaro ave bonito"
+    }
+];
